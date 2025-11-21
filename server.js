c2d7d635236069309cb2cca9e813183890cec1d9
SenMENSen <primecs3478@gmail.com>
2025-11-21 08:56:33 +0100

    server.js

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 const WebSocket = require('ws');
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    fs.readFile('./index.html', (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        res.end('Error loading index.html');
-        return;
-      }
-      res.writeHead(200);
-      res.end(data);
-    });
+    res.end(fs.readFileSync('index.html'));
   }
 });
-
 const wss = new WebSocket.Server({ server });
 const clients = new Set();
 
 wss.on('connection', ws => {
   clients.add(ws);
-
   ws.on('message', message => {
     for (let client of clients) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -29,11 +18,8 @@ wss.on('connection', ws => {
       }
     }
   });
-
-  ws.on('close', () => {
-    clients.delete(ws);
-  });
+  ws.on('close', () => { clients.delete(ws); });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Server on ${PORT}`));
